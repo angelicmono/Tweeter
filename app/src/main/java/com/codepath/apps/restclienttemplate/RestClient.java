127009282ff69e13d29079c9018d63106bd4745e
angelicmono <82055538+angelicmono@users.noreply.github.com>
2021-09-28 19:18:44 -0700

    updated OAuthBaseClient

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
@@ -52,6 +52,7 @@ public class RestClient extends OAuthBaseClient {
 		client.get(apiUrl, params, handler);
 	}
 
+
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
 	 * 	  i.e getApiUrl("statuses/home_timeline.json");
 	 * 2. Define the parameters to pass to the request (query or body)
